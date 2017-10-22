f8cb9f9d7bc5538436669bbd06a30086ffb90e7d
Karol Szurmański <karol.szurmanski@grupawp.pl>
2017-10-22 12:53:24 +0200

    fix charts- update

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,35 +1,31 @@
-import axios from 'axios';
+import axios from "axios";
 
 //Don't abuse my API please. You can generate yours for free - check README.md
-const API_KEY = '3476342d8797307b90b951f4614f0524';
+const API_KEY = "1e9e04d2a5b479bb7e8f9f9b7d9763e0";
 // Not so sneaky beaky trick to use the URL in both actions
 var ROOT_URL1 = `http://api.openweathermap.org/data/2.5/`;
 var ROOT_URL2 = `?appid=${API_KEY}&units=metric`;
 
-export const FETCH_WEATHER = 'FETCH_WEATHER';
-export const ONE_FETCH = 'ONE_FETCH';
+export const FETCH_WEATHER = "FETCH_WEATHER";
+export const ONE_FETCH = "ONE_FETCH";
 
 // To refactor with Middlewares
 
 export function oneWeather(city, country) {
+  const url = `${ROOT_URL1}weather${ROOT_URL2}&q=${city},${country}`;
+  const request = axios.get(url);
 
-    const url = `${ROOT_URL1}weather${ROOT_URL2}&q=${city},${country}`
-    const request = axios.get(url);
-
-    return {
-        type: ONE_FETCH, 
-        payload: request
-    }
-
+  return {
+    type: ONE_FETCH,
+    payload: request
+  };
 }
 export function fetchWeather(city, country) {
+  const url = `${ROOT_URL1}forecast${ROOT_URL2}&q=${city},${country}`;
+  const request = axios.get(url);
 
-    const url = `${ROOT_URL1}forecast${ROOT_URL2}&q=${city},${country}`
-    const request = axios.get(url);
-
-    return {
-        type: FETCH_WEATHER, 
-        payload: request
-    }
-
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
 }
